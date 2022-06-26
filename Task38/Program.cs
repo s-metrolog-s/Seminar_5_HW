d1fd57bb60c938b5bf1dba91c18248c4effbf468
Sergey <metrolog_omsk@mail.ru>
2022-06-26 11:08:40 +0500

    Решил задачу № 38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,3 +1,59 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
 
+Random rand = new Random();
+double[] array = new double[rand.Next(4, 10)];
+
+Console.Clear();
+Console.WriteLine("******************************************************************");
+Console.WriteLine($"Дан случайный массив из {array.Length} элементов:");
+
+MakeArray(array);
+PrintArray(array);
+
+Console.WriteLine("Находим разницу между максимальным и минимальным элементом...");
+Console.WriteLine($"Разница равна: {Math.Round(MinMaxDiff(array), 2)}");
+
+void MakeArray(double[] currentArray)
+{
+    for (int i = 0; i < currentArray.Length; i++)
+    {
+        currentArray[i] = Math.Round((rand.NextDouble() + rand.Next(1, 10)),2);
+    }
+}
+
+void PrintArray(double[] currentArray)
+{
+    for (int i = 0; i < currentArray.Length; i++)
+    {
+        Console.Write($"{currentArray[i]} \t");
+    }
+    Console.WriteLine();
+}
+
+double MinMaxDiff(double[] currentArray)
+{
+    double diff = 0;
+    double max = currentArray[0];
+    double min = currentArray[0];
+    int max_idx = 0;
+    int min_idx = 0;
+    for (int i = 0; i < currentArray.Length; i++)
+    {
+        if (currentArray[i] > max) 
+        {
+            max = currentArray[i];
+            max_idx = i;
+        }
+        if (currentArray[i] < min)
+        {
+            min = currentArray[i];
+            min_idx = i;
+        }
+        diff = currentArray[max_idx] - currentArray[min_idx];
+    }
+    return diff;
+}
+
+
+Console.WriteLine("******************************************************************");
